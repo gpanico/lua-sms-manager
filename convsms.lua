d0a94f43e0ea192a54cae0351db3a561cd8f3e71
gpanico <35564957+gpanico@users.noreply.github.com>
2021-11-04 10:29:28 +0100

    Create convsms.lua

---
+++ convsms.lua
@@ -0,0 +1,35 @@
+local socket = require "socket"
+local ip, port = "127.0.0.1", 23000
+local server, err = socket.connect(ip,port)
+
+if server == nil then
+  print (err)
+else
+  server:send('AT+CPMS="ME"\r\n')
+  s, status, partial = server:receive()
+  s, status, partial = server:receive()
+  s, status, partial = server:receive()
+  s, status, partial = server:receive()
+
+  server:send('AT+CMGR='..arg[1]..'\r\n')
+  s, status, partial = server:receive()
+  s, status, partial = server:receive()
+
+  while true do
+    s, status, partial = server:receive()
+    if status == "closed" then
+      break
+    end
+    if s == "OK" then
+      break
+    end
+
+    for i=1, #s do
+     local c=s:sub(i,i+1)
+     if math.mod(i,2) == 1 then
+       io.write(string.char(tonumber(c, 16)))
+     end
+    end
+
+  end
+end
