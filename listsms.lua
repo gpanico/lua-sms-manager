34dc46a586baedd7aa0f033d54a9e5a9bc1f958c
gpanico <35564957+gpanico@users.noreply.github.com>
2021-11-04 10:33:22 +0100

    Update listsms.lua

--- listsms.lua
+++ listsms.lua
@@ -1,12 +1,29 @@
+-- lists sms in memory
+-- if "-s" is provided as argument, also prints the number to which sms's were sent 
+
 local socket = require "socket"
 local ip, port = "127.0.0.1", 23000
 local server, err = socket.connect(ip,port)
+
+if arg[1] == "-s" then
+  -- get number from SIM
+  server:send('AT+CNUM\r\n')
+  s, status, partial = server:receive()
+  s, status, partial = server:receive()
+  j = string.gsub(s, '+CNUM: "","', '')
+  k = string.gsub(j, '",', ' ')
+  j = string.gsub(k, '145', '')
+  print ('Subject: SMS for ' .. j .. '\n')
+  s, status, partial = server:receive()
+  s, status, partial = server:receive()
+  s, status, partial = server:receive()
+end
 
 if server == nil then
   print (err)
 else
   -- set SMS text mode
-  server:send("AT+CMGF=1\r\n") 
+  server:send('AT+CMGF=1\r\n')
   s, status, partial = server:receive()
   s, status, partial = server:receive()
 
@@ -19,6 +36,8 @@ else
 
   -- list all messages
   server:send('AT+CMGL="ALL"\r\n')
+  s, status, partial = server:receive()
+
   while true do
     s, status, partial = server:receive()
     if status == "closed" then
